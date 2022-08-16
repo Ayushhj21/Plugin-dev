c9960b50978efe25e9f63c7d67555f65b72cfb01
Ayush Jain <ayushjain21.nmims@gmail.com>
2022-08-16 17:01:37 +0530

    fix:created a new folder

--- controllers/plugin.js
+++ controllers/plugin.js
@@ -12,6 +12,16 @@ const createPlugin = async (req, res) => {
 };
 
 
+const getAllTasks = async (req, res) => {
+    try {
+      const tasks = await Task.find({});
+      res.status(201).json({ tasks });
+    } catch (error) {
+      res.status(500).json({ msg: error });
+    }
+  };
+
+
 
 
   module.exports = {
